d36b55ee0daa575e60f2fa612bc94414be34dade
Juan Pablo Montoya <101369659+jmontoyavallejo@users.noreply.github.com>
2024-08-21 11:31:35 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-virtualenv --python=/usr/bin/python3.11 python 
+#!/bin/bash
+python3.11 -m venv python
 source python/bin/activate 
+pip install --upgrade pip
 pip install -r requirements.txt -t python/lib/python3.11/site-packages
 
 zip -r9 python.zip python
